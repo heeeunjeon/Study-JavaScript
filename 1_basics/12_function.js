a12263cd7c7e72d6e0a2795fd35ea16804e77a21
heeeun Jeon <syk03033@gmail.com>
2023-07-27 17:58:30 +0900

    return, Arrow, 즉시 실행 함수 study

--- 1_basics/12_function.js
+++ 1_basics/12_function.js
@@ -74,3 +74,90 @@ function multiply(x, y = 10) {
 multiply(2, 4); // 8 출력
 multiply(2); // 20 출력, y는 값을 넣어주지 않았기 때문에 defalut 값으로 적용
 
+console.log("-------------------------");
+
+
+
+/**
+ * 반환받기
+ * return 받기
+ */
+function multiply(x, y) {
+	return x * y; 
+}
+
+const result1 = multiply(2, 4);
+console.log(result1); // 8
+
+
+const result2 = multiply(4, 5);
+console.log(result2); // 20
+
+console.log("-------------------------");
+
+/**
+ * Arrow 함수
+ * 화살표 함수
+ */
+// 1번 방법
+const multiply2 = (x, y) => {
+	return x * y;
+}
+console.log(multiply2(3,4)); // 12
+
+// 2번 방법
+const multiply3 = (x,y) => x * y;
+console.log(multiply3(4,5)); // 20
+
+// 3번 방법
+const multiply4 = x => x * 2;
+console.log(multiply4(2)); // 8
+
+// 4번 방법
+const multiply5 = x => y => z => `x: ${x} y : ${y} : z : ${z}`;
+console.log(multiply5(2)(5)(7)); // x: 2 y : 5 : z : 7
+
+// 4번 방법을 일반 함수로 바꿔보기
+function multiply6(x) {
+	return function(y) {
+		return function(z) {
+			return `x: ${x} y : ${y} : z : ${z}`
+		}
+	}
+}
+console.log(multiply6(3)(4)(5)); // x: 3 y : 4 : z : 5
+
+// 이런식으로 일반함수와 섞어 써도 가능함
+const multiplyTwo = function(x, y) {
+	return x * y;
+}
+
+console.log(multiplyTwo(4, 5)); // 20
+
+const multiplyThree = function(x, y, z) {
+	console.log(arguments); // [Arguments] { '0': 4, '1': 5, '2': 6 }
+	return x * y * z;
+}
+console.log("-------------------------");
+console.log(multiplyThree(4, 5, 6)); // 120 
+
+
+console.log("-------------------------");
+const multiplyAll = function(...argument) { // 원하는 파라미터를 무한대로 받겠다라는 의미
+	return Object.values(argument).reduce((a, b) => a * b, 1);
+}
+console.log(multiplyAll(3, 4, 5, 6, 7, 8, 9, 10)); // 1814400 : parameter 들을 모두 곱한 값
+
+console.log("-------------------------");
+/**
+ * 즉시 실행 함수
+ * immediately invoked function 
+ */
+(function(x, y) {
+	console.log(x * y);
+}) (4, 5); // 20
+
+console.log(typeof multiply); // function
+console.log(multiply instanceof Object); // 좌측에 있는 것이 우측에 있는것과 같으냐? 라는 의미
+// 결과값 : true 
+// 자바스크립트에서 function은 Object임!! 
